bad7ecff24b7f15d8648ce7273514ba9ffc4b429
sasquarth <sasquarth@gmail.com>
2026-08-07 12:22:51 -0300

    chore: update pizicuia.png symbol icon and layout metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Kassino CKB - Fortune Tiger",
-  description: "Um clone exclusivo do Fortune Tiger para jogar com amigos usando CKBucks fictícios. Sem dinheiro real, apenas diversão e competição!",
+  description: "Sem dinheiro real, apenas diversão e competição!",
 };
 
 export default function RootLayout({
